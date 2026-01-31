7e1171edcc8e23ee56961a08bbe3f13cf2c12aff
Jonathan Blocksom <blocksom@gmail.com>
2026-01-31 14:02:13 -0500

    Adds vertical motion just for fun

--- main_player.gd
+++ main_player.gd
@@ -7,27 +7,39 @@ func _ready() -> void:
 	$AnimationPlayer.play("player_idle")
 	pass # Replace with function body.
 
+func _ensure_walking() -> void:
+	if is_idle:
+		$AnimationPlayer.play("player_walk")
+		is_idle = false
+
 func _process(delta: float) -> void:
-	var velocity = 0.0
+	var velocity = Vector2(0.0, 0.0)
 	
 	if Input.is_action_pressed("ui_left"):
-		velocity = -200.0
+		velocity.x = -200.0
 		flip_h = true
-		if is_idle:
-			$AnimationPlayer.play("player_walk")
-			is_idle = false
+		_ensure_walking()
+
 	elif Input.is_action_pressed("ui_right"):
-		velocity = 200.0
+		velocity.x = 200.0
 		flip_h = false
-		if is_idle:
-			$AnimationPlayer.play("player_walk")
-			is_idle = false
+		_ensure_walking()
+
+	elif Input.is_action_pressed("ui_down"):
+		velocity.y = 200.0
+		_ensure_walking()
+	
+	elif Input.is_action_pressed("ui_up"):
+		velocity.y = -200.0
+		_ensure_walking()
+
 	else:
+		# No keys pressed
 		if not is_idle:
 			$AnimationPlayer.play("player_idle")
 			is_idle = true
-
+			velocity = Vector2(0.0, 0.0)
 	
-	position.x += velocity * delta
+	position += delta * velocity
 
 	
