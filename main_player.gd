daa3958ff916b01ae30078c7241de5a3f76c0127
Jonathan Blocksom <blocksom@gmail.com>
2026-01-31 11:28:22 -0500

    Adds walking animation with HUD and BG updates

--- main_player.gd
+++ main_player.gd
@@ -3,10 +3,11 @@ extends Sprite2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	$AnimationPlayer.play("player_walk")
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	position.x += 1
+	position.x += 0.5
 	
